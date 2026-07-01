8e4c15fe6c1f791415c9e38bd013d3e5c88c5713
cathjcarney <cathjmurphy@hotmail.com>
2026-07-01 18:50:31 +1000

    Added code for index, script and styles

---
+++ script.js
@@ -0,0 +1,20 @@
+d3.csv("data/data_mod.csv").then(function(data) {
+
+    data.forEach(function(d) {
+        d.volume = +d.volume;
+    });
+
+    new d3plus.Treemap()
+        .data(data)
+        .groupby([
+            "level1",
+            "level2",
+            "level3"
+        ])
+        .sum("volume")
+        .label("label")
+        .color("level1")
+        .select("#treemap")
+        .render();
+
+});
