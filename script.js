cb3092e6fd24af05d93513bab37990b383c6d8dc
cathjcarney <cathjmurphy@hotmail.com>
2026-07-02 12:26:04 +1000

    fixing label error in script.js

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ d3.csv("data/data_mod.csv").then(function(data) {
             "level3"
         ])
         .sum("volume")
-        .label("label")
+        .label(function(d){
+            return d.label;
+        })
         .color("level1")
         .select("#treemap")
         .render();
